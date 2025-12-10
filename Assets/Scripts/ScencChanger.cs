c46c762e65278e92133c4a109743784dd788c869
aiden henderson <aidenhenderson@lcad.edu>
2025-12-10 12:41:14 -0800

    Added Start Screen with Button and Quit with Image

---
+++ Assets/Scripts/ScencChanger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScencChanger : MonoBehaviour
+{
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
